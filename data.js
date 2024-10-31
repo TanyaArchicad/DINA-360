01cf24d670e14d523a32ccfc70933654b1f4ce8e
TanyaArchicad <tanyadiz2014@gmail.com>
2024-10-31 17:39:20 +0500

    Upload

---
+++ data.js
@@ -0,0 +1,731 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-prihozhaya",
+      "name": "Prihozhaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.109730054386537,
+        "pitch": 0.0069696673690948785,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7962655747741163,
+          "pitch": -0.018712627712588414,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": 2.5342789253926963,
+          "pitch": 0.035926704861978465,
+          "rotation": 0,
+          "target": "2-kyhnya1"
+        },
+        {
+          "yaw": 2.8281299085501734,
+          "pitch": -0.05227880248107297,
+          "rotation": 0,
+          "target": "1-dushevaya1"
+        },
+        {
+          "yaw": -2.4002689063800826,
+          "pitch": 0.04703280989477321,
+          "rotation": 0,
+          "target": "6-koridor2"
+        },
+        {
+          "yaw": -2.2732975908388546,
+          "pitch": -0.1009091189572544,
+          "rotation": 0,
+          "target": "10-spalnyadevochki1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dushevaya1",
+      "name": "Dushevaya1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0055350845189999,
+        "pitch": -0.034132566308475276,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.096824330568868,
+          "pitch": 0.15661522930577654,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kyhnya1",
+      "name": "Kyhnya1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.1117060438064215,
+        "pitch": -0.04456823477571703,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.661595873747018,
+          "pitch": -0.011029377344318192,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": -0.8280451338366692,
+          "pitch": -0.18222485313087589,
+          "rotation": 0,
+          "target": "1-dushevaya1"
+        },
+        {
+          "yaw": 1.59675112247925,
+          "pitch": 0.010890491217356413,
+          "rotation": 0,
+          "target": "3-zal1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-zal1",
+      "name": "Zal1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5874739086696916,
+        "pitch": 0.003567655493940336,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5187542934395983,
+          "pitch": -0.038507311636685415,
+          "rotation": 0,
+          "target": "4-zal2"
+        },
+        {
+          "yaw": -1.3698284965121577,
+          "pitch": -0.21555606478239042,
+          "rotation": 0,
+          "target": "2-kyhnya1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-zal2",
+      "name": "Zal2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8109068216958288,
+        "pitch": -0.042936288180854376,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08821672775872713,
+          "pitch": 0.02567636259867534,
+          "rotation": 0,
+          "target": "3-zal1"
+        },
+        {
+          "yaw": -0.09261430559934958,
+          "pitch": -0.1326446696151038,
+          "rotation": 0,
+          "target": "2-kyhnya1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-koridor1",
+      "name": "Koridor1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6599401822207707,
+        "pitch": -0.019083302939147728,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3068455163778907,
+          "pitch": 0.0006675768789570213,
+          "rotation": 0,
+          "target": "10-spalnyadevochki1"
+        },
+        {
+          "yaw": 2.5730901808969753,
+          "pitch": 0.018769187798167053,
+          "rotation": 0,
+          "target": "12-spalnyaroditeley1"
+        },
+        {
+          "yaw": 0.3355729614783751,
+          "pitch": -0.004769852297513921,
+          "rotation": 0,
+          "target": "8-spalnyamalchika1"
+        },
+        {
+          "yaw": 0.02353422974839603,
+          "pitch": 0.012789170942792083,
+          "rotation": 0,
+          "target": "7-vanna"
+        },
+        {
+          "yaw": -1.601547154308541,
+          "pitch": 0.03173302534982625,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": 0.19872576439838063,
+          "pitch": 0.09043388374108119,
+          "rotation": 0,
+          "target": "6-koridor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-koridor2",
+      "name": "Koridor2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9371569853423001,
+        "pitch": 0.06720462497872859,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2262550647039667,
+          "pitch": 0.02612503616716566,
+          "rotation": 0,
+          "target": "7-vanna"
+        },
+        {
+          "yaw": 2.4926153121553423,
+          "pitch": 0.11386438504223406,
+          "rotation": 0,
+          "target": "8-spalnyamalchika1"
+        },
+        {
+          "yaw": -1.772234790570856,
+          "pitch": 0.10768631386008742,
+          "rotation": 0,
+          "target": "10-spalnyadevochki1"
+        },
+        {
+          "yaw": -1.5154249930208863,
+          "pitch": 0.021218726613101424,
+          "rotation": 0,
+          "target": "12-spalnyaroditeley1"
+        },
+        {
+          "yaw": -1.5325739941591738,
+          "pitch": 0.5172649770042757,
+          "rotation": 0,
+          "target": "5-koridor1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-vanna",
+      "name": "Vanna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5925991739112755,
+        "pitch": 0.21793373725958176,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2900750385623496,
+          "pitch": 0.22905649308858322,
+          "rotation": 0,
+          "target": "6-koridor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-spalnyamalchika1",
+      "name": "SpalnyaMalchika1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2727477929927957,
+        "pitch": -0.04106949304255636,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0679350219023807,
+          "pitch": -0.3983030315241862,
+          "rotation": 0,
+          "target": "9-spalnyamalchika2"
+        },
+        {
+          "yaw": -1.0321964531573116,
+          "pitch": -0.005940036215038802,
+          "rotation": 0,
+          "target": "6-koridor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-spalnyamalchika2",
+      "name": "SpalnyaMalchika2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2047292033584363,
+        "pitch": 0.37590465739364376,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9103606945240017,
+          "pitch": 0.3026198987380493,
+          "rotation": 0,
+          "target": "8-spalnyamalchika1"
+        },
+        {
+          "yaw": -2.7467849556840243,
+          "pitch": 0.15506210827359723,
+          "rotation": 0,
+          "target": "6-koridor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-spalnyadevochki1",
+      "name": "SpalnyaDevochki1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.488869773044149,
+        "pitch": -0.003097265226697843,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.568750717754753,
+          "pitch": -0.41985683433672705,
+          "rotation": 0,
+          "target": "11-spalnyadevochki2"
+        },
+        {
+          "yaw": 0.5073827835686782,
+          "pitch": -0.15728920531276103,
+          "rotation": 0,
+          "target": "5-koridor1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-spalnyadevochki2",
+      "name": "SpalnyaDevochki2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.337572327295014,
+          "pitch": -0.19341238746746292,
+          "rotation": 0,
+          "target": "10-spalnyadevochki1"
+        },
+        {
+          "yaw": 2.2256209469535158,
+          "pitch": -0.14649352598598142,
+          "rotation": 0,
+          "target": "5-koridor1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-spalnyaroditeley1",
+      "name": "SpalnyaRoditeley1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3977691217065171,
+        "pitch": 0.05320020190377406,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9627766583480835,
+          "pitch": -0.11067005020314014,
+          "rotation": 0,
+          "target": "14-garderobnaya"
+        },
+        {
+          "yaw": -1.6233224175568068,
+          "pitch": -0.027337574425123634,
+          "rotation": 0,
+          "target": "5-koridor1"
+        },
+        {
+          "yaw": 0.8692938376994341,
+          "pitch": -0.0677615399385445,
+          "rotation": 0,
+          "target": "13-spalnyaroditeley2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-spalnyaroditeley2",
+      "name": "SpalnyaRoditeley2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0711334865626316,
+        "pitch": -0.04666987845745041,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017411620577050257,
+          "pitch": -0.15816904471440196,
+          "rotation": 0,
+          "target": "14-garderobnaya"
+        },
+        {
+          "yaw": 0.43165427184381855,
+          "pitch": -0.08772274495327537,
+          "rotation": 0,
+          "target": "5-koridor1"
+        },
+        {
+          "yaw": -0.358652698555467,
+          "pitch": -0.023174422261323713,
+          "rotation": 0,
+          "target": "12-spalnyaroditeley1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-garderobnaya",
+      "name": "Garderobnaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.4955524570038676,
+        "pitch": 0.44512047465703475,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1848547861580734,
+          "pitch": 0.5829541472548208,
+          "rotation": 0,
+          "target": "12-spalnyaroditeley1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
